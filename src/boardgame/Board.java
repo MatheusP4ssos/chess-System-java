aaea97487a3fa81140a99a71067ccfe762568c63
Matheus Passos <matheus.senai2016@gmail.com>
2025-06-28 00:47:31 -0300

    BoardException and defensive programing

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -11,6 +11,10 @@ public class Board {
 
     // Construtor que inicializa o tabuleiro com dimensões específicas
     public Board(int rows, int columns) {
+        if (rows <= 0 || columns <= 0) {
+            //Especifica que o tabuleiro criado deve ter pelo menos uma linha e uma coluna
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         // Inicializa a matriz de peças com as dimensões fornecidas
@@ -22,31 +26,55 @@ public class Board {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
 
     // Métodos getters e setters para colunas
     public int getColumns() {
         return columns;
     }
 
-    public void setColumns(int columns) {
-        this.columns = columns;
-    }
 
     // Retorna a peça em uma posição específica usando linha e coluna
     public Piece piece(int row, int column) {
+        //Checa se a linha e coluna informadas existem no tabuleiro
+        if (!positionExists(row, column)) {
+            throw new BoardException("Error getting piece: position does not exist");
+        }
         return pieces[row][column];
     }
 
     // Sobrecarga do método piece que aceita um objeto Position
-    public Piece piece(Position position){
+    public Piece piece(Position position) {
+        //Checa se a posição informada existe no tabuleiro
+        if (!positionExists(position)) {
+            throw new BoardException("Error getting piece: position does not exist");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
-    public void placePiece(Piece piece, Position position){
+    public void placePiece(Piece piece, Position position) {
+        if (thereIsAPiece(position))
+            throw new BoardException("There is already a piece in that position " + position);
         pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    //Método para verificar se a posição existe no tabuleiro
+    private boolean positionExists(int row, int column) {
+        // linha solicitada precisa ser menor que a quantidade de linhas do tabuleiro e maior ou igual a 0
+        // coluna solicitada precisa ser menor que a quantidade de colunas do tabuleiro e maior ou igual a 0
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    // Caso as condições acimas sejam cumpridas ele retorna uma linha e coluna dentro do tabuleiro
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    //Caso a posição solicitada seja diferente de null ele retorna true
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
+        return piece(position) != null;
     }
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -15,6 +15,7 @@ public class ChessMatch {
     public ChessMatch() {
         // Cria um novo tabuleiro 8x8 (padrão do xadrez)
         board = new Board(8, 8);
+        //Organiza as peças na partida
         initialSetup();
     }
 
@@ -33,9 +34,10 @@ public class ChessMatch {
         return mat;
     }
 
+    // Cria um setup inicial colocando as peças em sua respectiva posição no tabuleiro
     private void initialSetup(){
         board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
-        board.placePiece((new King(board, Color.BLACK)), new Position(0,4));
+        board.placePiece((new King(board, Color.BLACK)), new Position(2,1));
         board.placePiece((new King(board, Color.WHITE)), new Position(7,4));
     }
 
